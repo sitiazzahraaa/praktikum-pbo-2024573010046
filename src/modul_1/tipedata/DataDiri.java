3bba64c2d4a01e0046fa93e589986d92ee060088
unknown <sitiazzahraaaaa@gmail.com>
2025-09-11 16:32:14 +0700

    DataDiri

---
+++ src/modul_1/tipedata/DataDiri.java
@@ -0,0 +1,27 @@
+package modul_1.tipedata;
+
+public class DataDiri {
+    public static void main(String[] args) {
+        String nama, alamat, agama;
+        int usia;
+        double tinggibadan;
+        char jk;
+        float beratbadan;
+
+        nama = "Siti Azzahra" ;
+        alamat = "Lhoksukon, Mns Pante" ;
+        agama = "Islam" ;
+        usia = 19 ;
+        tinggibadan = 155.5 ;
+        jk = 'P';
+        beratbadan = 45.2f ;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Agama: " + agama);
+        System.out.println("Usia: " + usia);
+        System.out.println("Tinggibadan: " + tinggibadan);
+        System.out.println("Jk: " + jk);
+        System.out.println("Beratbadan: " + beratbadan);
+    }
+}
